b3795a2ab723c95be11794df08402735de4a6ff3
Baurzhan Kiyakov <BKiyakov@instream.local>
2019-07-23 12:13:35 +0300

    functions requesting with user.name, auth is working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,65 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // // 🔥
-    // // The Firebase SDK is initialized and available here!
-    //
-    // firebase.auth().onAuthStateChanged(user => { });
-    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-    // firebase.messaging().requestPermission().then(() => { });
-    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-    //
+'use strict';
 
-    console.log("It works!");
+// Initializes the App.
+function App() {
+  document.addEventListener('DOMContentLoaded', function() {
+    // Variable for url + user.uid
+    this.userDataUrl = 'http://localhost:5001/do-it-app-1b4c7/us-central1/app/global';
 
-    //get elements
-    const signInBtn = document.querySelector("#signInBtn");
-    const signUpBtn = document.querySelector("#signUpBtn");
-    const signOutBtn = document.querySelector("#signOutBtn");
-    const emailInput = document.querySelector("#inputEmail");
-    const passInput = document.querySelector("#inputPass");
+    // Shortcuts to DOM Elements.
+    this.signInButton = document.getElementById('google-btn');
+    this.signOutButton = document.getElementById('sign-out-btn');
+    this.responseContainer = document.getElementById('response-container');
+    this.responseContainerCookie = document.getElementById('response-cookie-container');
+    this.urlContainer = document.getElementById('url-container');
+    this.urlContainerCookie = document.getElementById('url-cookie');
+    //this.usersDataUrl = window.location.href + 'users/';
+    //this.signedOutCard = document.getElementById('App-signed-out-card');
+    //this.signedInCard = document.getElementById('App-signed-in-card');
 
-    signUpBtn.addEventListener('click', () => {
-        const email = emailInput.value;
-        const pass = passInput.value;
-        firebase.auth().createUserWithEmailAndPassword(email, pass)
-        .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ...
-          });
-    })
+    // Bind events.
+    this.signInButton.addEventListener('click', this.signIn.bind(this));
+    this.signOutButton.addEventListener('click', this.signOut.bind(this));
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged.bind(this));
+  }.bind(this));
+}
 
-    signInBtn.addEventListener('click', () => {
-        const email = emailInput.value;
-        const pass = passInput.value;
-        firebase.auth().signInWithEmailAndPassword(email, pass)
-        .then(user => {
-            fetch(`https://us-central1-do-it-app-1b4c7.cloudfunctions.net/getUserData?user=${user.uid}`)
-            .then(data => {
-                console.log(data);
-            })
-        })
-        .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ...
-          });
-    })
+// Triggered on Firebase auth state change.
+App.prototype.onAuthStateChanged = function(user) {
+  if (user) {
+    this.urlContainer.textContent = this.userDataUrl;
+    this.urlContainerCookie.textContent = this.userDataUrl;
+    this.signInButton.style.display = 'none';
+    this.signOutButton.style.display = 'block';
+    this.startFunctionsRequest();
+    this.startFunctionsCookieRequest();
+  } else {
+    this.signInButton.style.display = 'block';
+    this.signOutButton.style.display = 'none';
+  }
+};
 
-    signOutBtn.addEventListener('click', () => {
-        firebase.auth().signOut();
-    })
+// Initiates the sign-in flow using GoogleAuthProvider sign in in a popup.
+App.prototype.signIn = function() {
+  firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+};
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid);
-            signOutBtn.classList.remove('hide');
-            
-        } else {
-            console.log("Not logged in");
-            signOutBtn.classList.add('hide');
-        }
-    })
-    // // 🔥
-  });
+// Signs-out of Firebase.
+App.prototype.signOut = function() {
+  firebase.auth().signOut();
+  // clear the __session cookie
+  document.cookie = '__session=';
+};
+
+// Does an authenticated request to a Firebase Functions endpoint using an Authorization header.
+App.prototype.startFunctionsRequest = function() {
+  firebase.auth().currentUser.getIdToken().then(function(token) {
+    console.log('Sending request to', this.userDataUrl , 'with ID token in Authorization header.');
+    var req = new XMLHttpRequest();
+    req.onload = function() {
+      this.responseContainer.innerText = req.responseText;
+    }.bind(this);
+    req.onerror = function() {
+      this.responseContainer.innerText = 'There was an error';
+    }.bind(this);
+    req.open('GET', this.userDataUrl , true);
+    req.setRequestHeader('Authorization', 'Bearer ' + token);
+    req.send();
+  }.bind(this));
+};
+
+// Does an authenticated request to a Firebase Functions endpoint using a __session cookie.
+App.prototype.startFunctionsCookieRequest = function() {
+  // Set the __session cookie.
+  firebase.auth().currentUser.getIdToken(true).then(function(token) {
+    // set the __session cookie
+    document.cookie = '__session=' + token + ';max-age=3600';
+
+    console.log('Sending request to', this.userDataUrl, 'with ID token in __session cookie.');
+    var req = new XMLHttpRequest();
+    req.onload = function() {
+      this.responseContainerCookie.innerText = req.responseText;
+    }.bind(this);
+    req.onerror = function() {
+      this.responseContainerCookie.innerText = 'There was an error';
+    }.bind(this);
+    req.open('GET', this.userDataUrl, true);
+    req.send();
+  }.bind(this));
+};
+
+// Load the App.
+window.App = new App();
